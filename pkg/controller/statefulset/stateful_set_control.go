9a18f1fb8ffb1611747f8e9b0dd9744bf5242e48
Lan Liang <gcslyp@gmail.com>
2023-09-05 09:55:50 +0000

    revert

--- pkg/controller/statefulset/stateful_set_control.go
+++ pkg/controller/statefulset/stateful_set_control.go
@@ -321,7 +321,9 @@ type replicaStatus struct {
 func computeReplicaStatus(pods []*v1.Pod, minReadySeconds int32, currentRevision, updateRevision *apps.ControllerRevision) replicaStatus {
 	status := replicaStatus{}
 	for _, pod := range pods {
-		status.replicas++
+		if isCreated(pod) {
+			status.replicas++
+		}
 
 		// count the number of running and ready replicas
 		if isRunningAndReady(pod) {
